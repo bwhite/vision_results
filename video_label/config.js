f4fcf2cbd11d9eb4f08002b6a3b79990dd3f9e09
Brandyn A. White <bwhite@dappervision.com>
2012-01-17 16:43:27 -0500

    Initial Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

---
+++ video_label/config.js
@@ -0,0 +1,10 @@
+{"title": "Video Annotator",
+ "image_name": "Frames (from a single video)",
+ "classes_name": "Events",
+ "class_description_name": "Event Description",
+ "footer": "<p>(c) 2012</p>",
+ "instructions": "<pre>Select the most appropriate video event using the dropdown box for the selected video frames.  A description is provided for each video event type.</pre>",
+ "classes": {"v0": {"description": "<pre>A video v0</pre>"},
+             "v1": {"description": "<pre>A video v1</pre>"},
+             "v2": {"description": "<pre>A video v1</pre>"}}}
+             
---
+++ video_label/data.js
@@ -0,0 +1,3 @@
+{"images": [{"src": "http://farm4.static.flickr.com/3267/3112736300_03ee1bb778.jpg", "width": 150},
+            {"src": "http://farm4.static.flickr.com/3267/3112736300_03ee1bb778.jpg", "width": 150},
+            {"src": "http://farm4.static.flickr.com/3267/3112736300_03ee1bb778.jpg", "width": 150}]}
