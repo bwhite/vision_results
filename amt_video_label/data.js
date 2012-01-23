7492d89452bf95767a64a70b343d7e12676bd702
Brandyn A. White <bwhite@dappervision.com>
2012-01-23 13:21:39 -0500

    Moved to mturk_vision project Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- amt_video_label/data.js
+++
@@ -1,3 +0,0 @@
-{"images": [{"src": "http://farm4.static.flickr.com/3267/3112736300_03ee1bb778.jpg", "width": 150},
-            {"src": "http://farm4.static.flickr.com/3267/3112736300_03ee1bb778.jpg", "width": 150},
-            {"src": "http://farm4.static.flickr.com/3267/3112736300_03ee1bb778.jpg", "width": 150}]}
